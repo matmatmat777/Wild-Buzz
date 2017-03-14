43a101584c8c52145c0d952d800c402d75844168
Mathieu <matdelmas@gmail.com>
2017-03-14 15:22:52 +0100

    architecture de base

---
+++ app/src/main/java/com/example/apprenti/wildbuzz/ChallengeB.java
@@ -0,0 +1,13 @@
+package com.example.apprenti.wildbuzz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ChallengeB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_challenge_b);
+    }
+}
