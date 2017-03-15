4b3ed65eb3f929c70a85a4ddd677ac85de9de1c2
omarWCS <omar.mbengue31000@gmail.com>
2017-03-15 07:28:08 +0100

    contrib_omar

---
+++ app/src/main/java/fr/wcs/wildbuzz/List.java
@@ -0,0 +1,52 @@
+package fr.wcs.wildbuzz;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+public class List extends AppCompatActivity implements View.OnClickListener {
+
+
+    ImageButton buttonChallengeA;
+    ImageButton buttonChallengeB;
+    ImageButton buttonChallengeC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+        buttonChallengeA=(ImageButton)findViewById(R.id.buttonChallengeA);
+        buttonChallengeB=(ImageButton)findViewById(R.id.buttonChallengeB);
+        buttonChallengeC=(ImageButton)findViewById(R.id.buttonChallengeC);
+        buttonChallengeA.setOnClickListener(this);
+        buttonChallengeB.setOnClickListener(this);
+        buttonChallengeB.setOnClickListener(this);
+    }
+
+    public void OnClick(View view) {
+        switch (view.getId()) {
+
+            case R.id.buttonChallengeA:
+                Intent buttonA = new Intent(List.this, ChallengeA.class);
+                List.this.startActivity(buttonA);
+                break;
+
+            case R.id.buttonChallengeB:
+                Intent buttonB = new Intent(List.this, ChallengeB.class);
+                List.this.startActivity(buttonB);
+                break;
+
+            case R.id.buttonChallengeC:
+                Intent buttonC = new Intent (List.this, ChallengeC.class);
+                List.this.startActivity(buttonC);
+                break;
+
+            default:
+                break;
+        }
+
+    }
+}
